a219d88dbae101ad5addebf319750f08343d1eb1
rralfafara <57926333+rralfafara@users.noreply.github.com>
2019-11-22 11:27:16 +0900

    Initial code for displaying results

--- public/js/index.js
+++ public/js/index.js
@@ -47,12 +47,12 @@ function run_indexer(){
 function wait_indexer_finished(){
     get_indexer_status( function(result) {
             if (result == "inProgress"){
-                $(".indexerStatus h3").html("Indexer Execution in Progress...");
+                $("#indexerStatus h3").html("Indexer Execution in Progress...");
                 setTimeout(wait_indexer_finished, 5000);
             }else if(result == "success"){
-                $(".indexerStatus h3").html("Indexer Execution Complete.");
+                $("#indexerStatus h3").html("Indexer Execution Complete.");
             }else{
-                $(".indexerStatus h3").html("Error in Indexer Execution.");
+                $("#indexerStatus h3").html("Error in Indexer Execution.");
             }    
     });
 }
@@ -75,4 +75,32 @@ function get_indexer_status(callback){
             callback("error");
         }
     });
+}
+
+//To confirm
+function display_resultsLabel(results){
+    if (results.length > 0) {
+        $("#results_label").html("Results");
+    }
+}
+
+function display_results(results){
+    var new_result = $(".res.template").clone().css("display", "block").attr("class", "res");
+    var new_filename = $(".filename").clone().css("display", "block").attr("class", "filename");
+    var results_filename = "";
+    new_filename.find("p").html(results_filename);
+    var new_filesnippet = $(".snippet").clone().css("display", "block").attr("class", "snippet");
+    var results_snippet = "";
+    new_filesnippet.find("p").html(results_snippet);
+
+    //Confirm if this will work
+    $(".res").append(new_filename);
+    $(".res").append(new_filesnippet);
+    $("#results").append(new_result);
+}
+
+function display_filePreview(src, filename, text){
+    $("#file_preview").html("<iframe src=\'+src+\">" + filename + "</iframe>");
+    $("#textfile").html(text);
+    $("#files_modal").modal('show');
 }
