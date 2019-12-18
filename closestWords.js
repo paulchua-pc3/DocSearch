913bed8ccaac6276d11321cdc369a16cbb3efbe4
Mark Jason Chua <markjason.chua@ubicom-hd.com>
2019-12-18 18:22:30 +0900

    switch to jaro-winkler similarity

--- closestWords.js
+++ closestWords.js
@@ -8,13 +8,15 @@ module.exports = function(query) {
     var list = words.slice(0);    
     
     //using levenshtein distance
+    /*
     const levenshtein = require('fast-levenshtein');
     list.sort((a, b) => levenshtein.get(query,a) - levenshtein.get(query,b));
-
+    */
+   
     //using jaro-winkler similarity
-    /*
+    
     const similarity = require('jaro-winkler');
     list.sort((a, b) => similarity(query,b) - similarity(query,a));
-    */
+    
     return list.slice(0,3);
 }
