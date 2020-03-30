c52bf2e3b0f9cb819fab5f3f41bb88fc354b97ec
paulchua-pc3 <59995377+paulchua-pc3@users.noreply.github.com>
2020-03-30 18:00:52 +0800

    updated watson classification response handling

--- waCaller.js
+++ waCaller.js
@@ -1,38 +1,39 @@
-module.exports = function(query, callback) {
-    const settings = require('./settings.json');
-    const config = settings.watson_classify;
-  
-    const AssistantV1 = require('ibm-watson/assistant/v1');
+module.exports = function (query, filename, callback) {
+  const settings = require('./settings.json');
+  const path = require('path');
+  const config = settings.watson_classify;
 
-    const { IamAuthenticator } = require('ibm-watson/auth');
-  
-    const assistant = new AssistantV1({
-      url: config.url,
-      version: config.version,
-      authenticator: new IamAuthenticator({ apikey: config.apikey })
-    }); 
-    //console.log("query "+query);
-    //console.log("before query");
-  
-    var params = {
-      workspaceId: config.workspace_id
-    };   
+  const AssistantV1 = require('ibm-watson/assistant/v1');
 
-    params.input = { "text": query };
-    
-    //console.log("params: ",JSON.stringify(params));
-    assistant.message(params,
-      function(err, response) {
-        if (err) {
-          console.error(err);
-        } else {
-          console.log(JSON.stringify(response.result, null, 2));
-          if(response.result.intents.length > 0){
+  const { IamAuthenticator } = require('ibm-watson/auth');
+
+  const assistant = new AssistantV1({
+    url: config.url,
+    version: config.version,
+    authenticator: new IamAuthenticator({ apikey: config.apikey })
+  });
+
+  var params = {
+    workspaceId: config.workspace_id
+  };
+
+  params.input = { "text": query };
+
+  assistant.message(params,
+    function (err, response) {
+      if (err) {
+        console.error(err);
+      } else {        
+        if (response.result.intents.length > 0) {
+          if (response.result.intents[0].confidence >= 0.9) {
             callback(response.result.intents[0].intent);
-          }else{
+          } else {
             callback("Irrelevant");
-          }          
+          }
+        } else {
+          callback("Irrelevant");
         }
       }
-    );
-  };
+    }
+  );
+};
